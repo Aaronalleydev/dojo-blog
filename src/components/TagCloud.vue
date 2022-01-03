a92bd7f403912488c4b737dc485ae3e4929cd642
Aaron Alley <aaronalleydev@gmail.com>
2022-01-03 13:47:13 +0000

    finished the blog project

---
+++ src/components/TagCloud.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="tag-cloud">
+    <h3>Tags</h3>
+    <div v-for="tag in tags" :key="tag" class="tagLinks">
+      <router-link :to="{ name: 'Tag', params: { tag: tag}}">
+        #{{ tag }}
+      </router-link>
+    </div>
+    <div>
+
+    </div>
+  </div>
+</template>
+
+<script>
+
+import useTags from '../composoables/useTags'
+export default {
+  props: ['posts'],
+  setup(props) {
+    const { tags } = useTags(props.posts)
+
+    return { tags }
+  }
+}
+</script>
+
+<style>
+  .tagLinks a {
+    text-decoration: none;
+    color: grey;
+    cursor: pointer;
+  }
+ .tagLinks {
+   display: inline-block;
+   margin-left: 10px;
+ }
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,8 +4,9 @@
     <div v-if="error">
       {{error}}
     </div>
-    <div v-if="posts.length" >
+    <div v-if="posts.length" class="layout" >
       <PostList :posts="posts"/>
+      <TagCloud :posts="posts"/>
     </div>
     <div v-else>
       <Spinner />
@@ -17,10 +18,11 @@
 import PostList from '../components/PostList.vue'
 import getPosts from '../composoables/getPosts'
 import Spinner from '../components/Spinner.vue'
+import TagCloud from '../components/TagCloud.vue'
 
 export default {
   name: 'Home',
-  components: { PostList, Spinner },
+  components: { PostList, Spinner, TagCloud },
   setup() {
    const {posts, error, load} = getPosts()
 
@@ -36,6 +38,12 @@ export default {
   max-width: 1200px;
   margin: 0 auto;
   padding: 10px;
+}
+
+.layout {
+  display: grid;
+  grid-template-columns: 3fr 1fr;
+  gap: 20px;
 }
   
 </style>
